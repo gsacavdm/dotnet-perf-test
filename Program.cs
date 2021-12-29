16c9f6fc19a0f6f22537afb48a15f31f0cda8e4e
Gerardo Saca <gsacavdm@microsoft.com>
2021-12-28 23:56:27 -0800

    Fix stopwatch and refactor Duration

--- Program.cs
+++ Program.cs
@@ -22,45 +22,61 @@ namespace dotnet_perf_test
             if (options == null) throw new ArgumentNullException(nameof(options));
 
             var testResults = new List<TestResult>();
+            var testDurations = new Dictionary<string, List<double>>();
+            var stopwatch = new Stopwatch();
 
             foreach(var testName in options.Tests.Split(','))
             {
-              Console.WriteLine($"Running test {testName}");
-              var stopwatch = new Stopwatch();
-  
+              Console.WriteLine($"Running test {testName} {options.Runs} times with {options.Items} items");
+              testDurations.Add(testName, new List<double>());
+
               for (var run = 0; run < options.Runs; run++)
               {
                 var test = (PerformanceTest)(Activator.CreateInstance(null, "dotnet_perf_test.Tests." + testName).Unwrap());
                 test.PreTest(options);
-    
+                
+                stopwatch.Reset();
                 stopwatch.Start();
                 test.Test();
                 stopwatch.Stop();
     
                 test.PostTest();
-    
+  
+                var duration = stopwatch.Elapsed.TotalSeconds;
+
                 testResults.Add(
                   new TestResult
                   {
                     TestName = testName,
                     Run = run,
                     Items = options.Items,
-                    Duration = stopwatch.Elapsed
-                  }
+                    Duration = duration                  }
                 );
+
+                testDurations[testName].Add(duration);
               }
+
+              testDurations[testName].Sort();
             }
 
-            var testSummaries = testResults.GroupBy(t => t.TestName, t => t)
+            Console.WriteLine("====== Test Results =======");
+            int percentile90Index = (int)(options.Runs * .9);
+
+
+            var testSummaries = testResults
+              .GroupBy(t => t.TestName, t => t)
               .Select(g => new {
-              TestName = g.Key,
-              Runs = g.Count(),
-              Average = g.Average(ta => ta.Duration.TotalSeconds)
-            });
+                TestName = g.Key,
+                Runs = g.Count(),
+                //DurationsUnsorted = String.Join(',',  g.Select(gg => gg.Duration)),
+                //DurationsSorted = String.Join(',', testDurations[g.Key]),
+                Percentile90 = testDurations[g.Key].ElementAt(percentile90Index),
+                Average = g.Average(ta => ta.Duration)
+              });
 
             foreach(var testSummary in testSummaries)
             {
-              Console.WriteLine($"Test {testSummary.TestName} | Runs {testSummary.Runs} | Average (in Seconds) {testSummary.Average}");
+              Console.WriteLine($"Test {testSummary.TestName} | Runs {testSummary.Runs} | Average (in Seconds) {testSummary.Average} | Percentile90 {testSummary.Percentile90}"); //Durations {testSummary.Durations}");
             }
         }
     }
--- TestResult.cs
+++ TestResult.cs
@@ -7,6 +7,6 @@ namespace dotnet_perf_test
       public string TestName { get; set; }
       public int Run { get; set; }
       public int Items { get; set; }
-      public TimeSpan Duration { get; set; }
+      public double Duration { get; set; }
     }
 }
