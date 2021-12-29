a8b6bae7f8b5b06cc86eea8300f7c9fe2516aa8f
Gerardo Saca <gsacavdm@microsoft.com>
2021-12-28 16:13:16 -0800

    Add InsertToOrderedDictionaryTest

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using CommandLine;
 
@@ -7,36 +8,24 @@ namespace dotnet_perf_test
 {
     class Program
     {
-        public class Options
-        {
-          [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-          public bool Verbose { get; set; }
-          
-          [Option('l', "limit", Required = false, Default=10, HelpText = "TBD")]
-          public int Limit { get; set; }
-
-          [Option('i', "iterations", Required = false, Default=1, HelpText = "Test iterations")]
-          public int Iterations { get; set; }
-        }
-
-        static Options ProgramOptions { get; set; }
-
         static void Main(string[] args)
         {
+            Options options = null;
             Parser.Default.ParseArguments<Options>(args)
               .WithParsed<Options>(o => 
               {
-                ProgramOptions = o;
+                options = o;
               });
 
-            Console.WriteLine("Hello World!");
+            if (options == null) throw new ArgumentNullException(nameof(options));
 
+            Console.WriteLine($"Running test {options.Test}");
             var stopwatch = new Stopwatch();
 
-            for (var i = 0; i < ProgramOptions.Iterations; i++)
+            for (var i = 0; i < options.Iterations; i++)
             {
-              var test = new InsertToListTest { Limit = ProgramOptions.Limit };
-              test.PreTest();
+              var test = (PerformanceTest)(Activator.CreateInstance(null, "dotnet_perf_test." + options.Test).Unwrap());
+              test.PreTest(options);
   
               stopwatch.Start();
               test.Test();
@@ -49,9 +38,30 @@ namespace dotnet_perf_test
         }
     }
 
+    public class Options
+    {
+      [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+      public bool Verbose { get; set; }
+      
+      [Option('l', "limit", Required = false, Default=10, HelpText = "TBD")]
+      public int Limit { get; set; }
+
+      [Option('i', "iterations", Required = false, Default=1, HelpText = "Test iterations")]
+      public int Iterations { get; set; }
+
+      [Option('t', "test", Required = true, HelpText = "Name of test to run")]
+      public string Test { get; set; }
+    }
+
     public abstract class PerformanceTest
     {
-      public virtual void PreTest() {}
+      protected Options Options { get; set; }
+
+      public virtual void PreTest(Options options)
+      {
+        this.Options = options;
+      }
+
       public abstract void Test();
       public virtual void PostTest() {}
     }
@@ -67,6 +77,23 @@ namespace dotnet_perf_test
         for (var i = 0; i < this.Limit; i++)
         {
           this.TheList.Add(i.ToString());
+        }
+      }
+
+    }
+
+    public class InsertToOrderedDictionaryTest : PerformanceTest
+    {
+      public int Limit { get; set; }
+
+      private readonly OrderedDictionary TheList = new OrderedDictionary();
+
+      public override void Test()
+      {
+        for (var i = 0; i < this.Limit; i++)
+        {
+          var tmp = i.ToString();
+          this.TheList.Add(tmp, tmp);
         }
       }
 
