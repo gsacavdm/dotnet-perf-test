a80b0b065c8acc812d5b1731cdfaaf11ef644c80
Gerardo Saca <gsacavdm@microsoft.com>
2021-12-31 09:00:28 -0800

    Increase defaults for runs and items

--- src/Options.cs
+++ src/Options.cs
@@ -8,10 +8,10 @@ namespace dotnet_perf_test
       [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
       public bool Verbose { get; set; }
       
-      [Option('r', "runs", Required = false, Default=10, HelpText = "Number of times each test is run")]
+      [Option('r', "runs", Required = false, Default=100, HelpText = "Number of times each test is run")]
       public int Runs { get; set; }
 
-      [Option('i', "items", Required = false, Default=100, HelpText = "Number of items the test should use")]
+      [Option('i', "items", Required = false, Default=1000, HelpText = "Number of items the test should use")]
       public int Items { get; set; }
 
       [Option('t', "tests", Required = true, HelpText = "Comma-delimited list of tests to run")]
