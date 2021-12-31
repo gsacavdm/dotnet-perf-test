c4f017bf1a9f2962559fca877bdf8677313cf7f0
Gerardo Saca <gsacavdm@microsoft.com>
2021-12-31 09:00:43 -0800

    Add InsterToPreAllocatedListTest

---
+++ src/Tests/InsertToPreAllocatedListTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet_perf_test.Tests
+{
+    public class InsertToPreAllocatedListTest : PerformanceTest
+    {
+      private List<int> TheList = new();
+      public override void PreTest(Options options)
+      {
+        this.TheList = new List<int>(options.Items);
+        base.PreTest(options);
+      }
+
+      public override void Test()
+      {
+        if (this.TheList == null) throw new Exception("Test hasn't been initialized. Please call PreTest() first before calling Test()");
+
+        for (var i = 0; i < this.Options.Items; i++)
+        {
+          this.TheList.Add(i);
+        }
+      }
+    }
+}
